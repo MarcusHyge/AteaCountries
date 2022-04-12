670215f8cc762fa93191349414d55813e3c26ea2
Hyge <marcushyge25@gmail.com>
2022-04-12 08:40:26 +0200

    Refactoring; Split up the main function to smaller functions to create modularity

--- Atea/src/main.go
+++ Atea/src/main.go
@@ -9,6 +9,9 @@ import (
 	"net/http"
 )
 
+const apiurl = "https://countries.trevorblades.com/"
+const apimethod = http.MethodPost
+
 type MainData struct {
 	Data struct {
 		Countries []struct {
@@ -18,6 +21,14 @@ type MainData struct {
 			} `json:"languages"`
 		} `json:"countries"`
 	} `json:"data"`
+}
+
+func ConvertToJson(jsonData map[string]string) []byte {
+	body, err := json.Marshal(jsonData)
+	if err != nil {
+		panic(err)
+	}
+	return body
 }
 
 func GetCountriesByCurrency() bool {
@@ -35,44 +46,56 @@ func GetCountriesByCurrency() bool {
         `,
 	}
 
-	//Convert to json
-	body, err := json.Marshal(jsonData)
-	if err != nil {
-		panic(err)
-	}
-
 	//Make request
-	request, err := http.NewRequest("POST", "https://countries.trevorblades.com/", bytes.NewBuffer(body))
-	if err != nil {
-		fmt.Errorf("Error creating new request: ", err.Error())
+	request := CreateRequest(jsonData)
+	response := RequestData(request)
+	defer response.Body.Close()
+	data := ReadResponseBody(response)
+	result := JsonDataToStruct(data)
+	hashMap := GroupCountriesByName(result)
+	PrintOutValuesOfMap(hashMap)
+	return true
+}
+
+func ReadResponseBody(response *http.Response) []byte {
+	data, _ := ioutil.ReadAll(response.Body)
+	return data
+}
+
+func JsonDataToStruct(data []byte) MainData {
+	var result MainData
+	if err := json.Unmarshal(data, &result); err != nil {
+		fmt.Errorf("Can not unmarshal JSON:", err.Error())
 	}
+	return result
+}
 
-	//Set content type
-	request.Header.Add("content-type", "application/json")
-
-	//Request data
+func RequestData(request *http.Request) *http.Response {
 	client := &http.Client{}
 	response, err := client.Do(request)
 	if err != nil {
 		fmt.Errorf("Response error:", err.Error())
 	}
-	//Close body
-	defer response.Body.Close()
-	data, _ := ioutil.ReadAll(response.Body)
+	return response
+}
 
-	//Put the json data in a struct
-	var result MainData
-	if err := json.Unmarshal(data, &result); err != nil {
-		fmt.Errorf("Can not unmarshal JSON:", err.Error())
+func CreateRequest(jsonData map[string]string) *http.Request {
+	request, err := http.NewRequest(http.MethodPost, apiurl, bytes.NewBuffer(ConvertToJson(jsonData)))
+	if err != nil {
+		fmt.Errorf("Error creating new request: ", err.Error())
 	}
+	//Set content type
+	request.Header.Add("content-type", "application/json")
+	return request
+}
 
+func GroupCountriesByName(result MainData) map[string][]string {
 	/*
 		Making map to group values.
 		Key Values = Languages
 		Elements = Countries
 	*/
 	hashMap := make(map[string][]string)
-
 	//For each country object
 	for _, Country := range result.Data.Countries {
 		//For each language object
@@ -83,11 +106,13 @@ func GetCountriesByCurrency() bool {
 			hashMap[Language.Name] = CountryList
 		}
 	}
-	//Loop through the map and print the grouped values
+	return hashMap
+}
+
+func PrintOutValuesOfMap(hashMap map[string][]string) {
 	for key, element := range hashMap {
 		fmt.Println("Country:", key, "=>", "Languages:", element)
 	}
-	return true
 }
 
 func main() {
